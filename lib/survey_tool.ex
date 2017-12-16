570364e6253fbed5b629f648e997fbfc9c1bf0c6
Andrew Towers <atowers@gmail.com>
2017-12-16 14:50:05 +1100

    Initial commit

---
+++ lib/survey_tool.ex
@@ -0,0 +1,5 @@
+defmodule SurveyTool do
+  @moduledoc """
+  Documentation for SurveyTool.
+  """
+end
---
+++ lib/survey_tool/cli.ex
@@ -0,0 +1,10 @@
+defmodule SurveyTool.CLI do
+
+  @moduledoc """
+  Entry point for your command line application.
+  """
+
+  def main(_args) do
+    IO.puts "Welcome to Xplor's Coding Test"
+  end
+end
